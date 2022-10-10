95b791a12d819e46a2cd9d0fd2497581f4ce999e
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-10 22:01:33 +0300

    task2

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int n1=n/10000;
+int n2=n/1000-(n/10000)*10;
+int n4=(n%100)/10;
+int n5=n%10;
+
+if (9999<n && n<100000)
+{
+    if(n1==n5 && n2==n4)
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine("не Палиндром");
+    }
+}
+else 
+{
+    Console.WriteLine("Не пятизначное! Нужно пятизначное");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(" Введите координаты 1 точки");
+
+int x1=Convert.ToInt32(Console.ReadLine());
+int y1=Convert.ToInt32(Console.ReadLine());
+int z1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите координаты 2 точки");
+
+int x2=Convert.ToInt32(Console.ReadLine());
+int y2=Convert.ToInt32(Console.ReadLine());
+int z2=Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine($"Длинна отрезка - {Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1))}");
