97964f69c9ae3ac9121235d82bd03e2e72aa09e3
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-11 01:37:53 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ведите число");
+int num=Convert.ToInt32(Console.ReadLine());
+int k=0;
+for (int i=0; i<=num; i++)
+{
+  k = i*i*i; 
+  Console.Write($"{k}  " );
+}
+//return k;
